bd2488e36a8fbf130bb0b91c4ff3d3b21ffce7fc
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-08-28 11:47:11 +0100

    chore: Update database configuration for secure communication

--- config/database.py
+++ config/database.py
@@ -1,9 +1,23 @@
 from sqlmodel import Session, create_engine, SQLModel
+import os
+import pymysql
+from dotenv import load_dotenv
 
+load_dotenv()
+pymysql.install_as_MySQLdb()
 
-DATABASE_URL = "sqlite:///database.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+CA_CERT_PATH = os.getenv("CA_CERT_PATH")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    url=DATABASE_URL,
+    connect_args={
+        "ssl": {
+            "sslmode": "REQUIRED",
+            "ca": CA_CERT_PATH,
+        },
+    },
+)
 
 
 def get_db():
