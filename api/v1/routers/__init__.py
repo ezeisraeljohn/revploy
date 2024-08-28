e908913d743d38ee855d273fca0934ce1e386578
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-08-28 11:28:06 +0100

    Added the init file

---
+++ api/v1/routers/__init__.py
@@ -0,0 +1,4 @@
+from .employees import router as employee_router
+from .departments import router as department_router
+from .positions import router as position_router
+from .performance_reviews import router as performance_review_router
