c08838d714d0f95f059a780ad43b079d0c20ee0b
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-08-29 05:15:31 +0100

    chore: Refactor create_employee function to handle department and position validation

--- api/v1/crud/employee.py
+++ api/v1/crud/employee.py
@@ -12,11 +12,7 @@ from datetime import datetime
 
 def create_employee(employee: EmployeeCreate, db: Session):
     """Creates an employee"""
-    employee_dict = (
-        employee.model_dump()
-    )  # Use dict() to convert Pydantic model to dictionary
-
-    # Check if department_id is provided
+    employee_dict = employee.model_dump()
     if employee_dict.get("department_id"):
         department = db.exec(
             select(DepartmentDatabase).where(
@@ -49,7 +45,7 @@ def create_employee(employee: EmployeeCreate, db: Session):
         db.commit()
         db.refresh(new_employee)
     except Exception as e:
-        db.rollback()  # Rollback in case of error
+        db.rollback()
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"An error occurred while creating the employee: {str(e)}",
@@ -83,13 +79,36 @@ def update_employee(id: int, employee: EmployeeUpdate, db: Session):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="employee not found"
         )
-    employee = employee.model_dump()
-    for key, value in employee.items():
+    employee_dict = employee.model_dump()
+    if employee_dict.get("department_id") or employee_dict.get("department_id") == 0:
+        department = db.exec(
+            select(DepartmentDatabase).where(
+                DepartmentDatabase.id == employee_dict.get("department_id")
+            )
+        ).first()
+        if not department:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Department not found. Please enter a valid department id.",
+            )
+    if employee_dict.get("position_id") or employee_dict.get("position_id") == 0:
+        position = db.exec(
+            select(PositionDatabase).where(
+                PositionDatabase.id == employee_dict.get("position_id")
+            )
+        ).first()
+        if not position:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Position not found. Please enter a valid position id.",
+            )
+    for key, value in employee_dict.items():
         setattr(result, key, value)
     result.date_updated = datetime.now()
     db.commit()
+    db.refresh(result)
 
-    return employee
+    return result
 
 
 def delete_employee(id: int, db: Session):
